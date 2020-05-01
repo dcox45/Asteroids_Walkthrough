8bf52afd899522e55a23e28af0b621a6493b27c7
dcox45 <dylanwjc@gmail.com>
2020-05-01 13:55:43 -0400

    functional window, paint to be implemented

--- src/com/company/Game.java
+++ src/com/company/Game.java
@@ -1,4 +1,32 @@
 package com.company;
 
-public class Game {
+import javax.swing.*;
+import java.awt.*;
+
+public class Game extends JFrame {
+
+    public void init(){                                      // this method sets the initial conditions of the game
+        this.setVisible(true);
+        this.setSize(900, 600);                 // gives the game window a title
+        this.setTitle("Asteroids - by John Doe");           // gives the game window a title
+        this.setResizable(false);
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);   // freezes the size of the window
+    }
+
+    public void paint(Graphics g){
+        
+    }
+
+
 }
+
+
+
+
+// freezes the size of the window
+
+// gives the game window a title
+
+// sets the game window to 900 pixels wide and 600 in height
+
+// this method sets the initial conditions of the game
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,13 @@
 package com.company;
 
+
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Game asteroids = new Game();        // this  creates a new Game window that we call asteroids
+        asteroids.init();
     }
 }
+
+

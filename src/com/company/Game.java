b9bd14ba132f15510154171460122fd17a8a47eb
dcox45 <dylanwjc@gmail.com>
2020-05-05 15:55:22 -0400

    adds config files to git

--- src/com/company/Game.java
+++ src/com/company/Game.java
@@ -10,6 +10,7 @@ public class Game extends JFrame implements KeyListener {
     int x1, y1, x2, y2;     // these are the x and y coordinates of our line
     int[] x;                // declaring an array of x coordinates for our Polygon
     int[] y;                // and an array of y coordinates
+    VectorSprite ship;      // creates an instance of VectorSprite called "ship"
 
     public void init(){                                                          // this method sets the initial conditions of the game
         this.setVisible(true);
@@ -18,17 +19,13 @@ public class Game extends JFrame implements KeyListener {
         this.setResizable(false);
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);               // freezes the size of the window
         this.addKeyListener(this);
-        x = new int[3];
-        x[0] = 15;
-        x[1] = 0;
-        x[2] = 30;
-        y = new int[] {20, 50, 50};
+        ship = new VectorSprite();
     }
 
     public void paint(Graphics g){
         g.fillRect(0, 0, 900, 600);
         g.setColor(Color.GREEN);
-        g.drawPolygon(x, y, x.length);
+        ship.paint(g);
     }
 
     public void update(Graphics g){
@@ -43,8 +40,9 @@ public class Game extends JFrame implements KeyListener {
     @Override
     public void keyPressed(KeyEvent e) {
         if(e.getKeyCode() == KeyEvent.VK_RIGHT) {
-            x1 = x1 + 10;
-            x2 = x2 + 10;
+            x[0] += 10;
+            x[1] += 10;
+            x[2] += 10;
         }
         repaint();
     }
--- src/com/company/VectorSprite.java
+++ src/com/company/VectorSprite.java
@@ -1,4 +1,27 @@
 package com.company;
 
+import java.awt.*;
+
 public class VectorSprite {
+
+    Polygon shape;          // each VectorSprite will have a Polygon
+
+    double xposition;
+    double yposition;
+    double xspeed;
+    double yspeed;
+    double angle;
+
+    public VectorSprite() {
+        shape = new Polygon();
+
+        shape.addPoint(15, 20);
+        shape.addPoint(0, 50);
+        shape.addPoint(30, 50);
+    }
+
+    public void paint(Graphics g) {
+        g.drawPolygon(shape);
+    }
+
 }

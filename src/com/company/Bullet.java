d18a8f8937c022a7dc71e5c793d4ce810826c7ba
dcox45 <dylanwjc@gmail.com>
2020-05-25 19:34:41 -0400

    9.1 Bullet Class created

---
+++ src/com/company/Bullet.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import java.awt.*;
+
+public class Bullet extends VectorSprite{
+
+    public Bullet(double shipX, double shipY, double shipAngle) {
+        shape = new Polygon();
+
+        shape.addPoint(1, 1);
+        shape.addPoint(-1, 1);
+        shape.addPoint(-1, -1);
+        shape.addPoint(1, -1);
+
+        drawShape = new Polygon();
+
+        drawShape.addPoint(1, 1);
+        drawShape.addPoint(-1, 1);
+        drawShape.addPoint(-1, -1);
+        drawShape.addPoint(1, -1);
+
+        xposition = shipX;
+        yposition = shipY;
+        angle = shipAngle;
+
+        THRUST = 10;
+        xspeed = Math.cos(angle) * THRUST;
+        yspeed = Math.sin(angle) * THRUST;
+    }
+
+}
